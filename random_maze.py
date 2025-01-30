b8437c88163ec77aaa5ac309dadd455b386c8783
IalvinchangI <99197625+IalvinchangI@users.noreply.github.com>
2025-01-30 19:03:27 +0800

    2-3 更多的終點
    模擬更多人在走迷宮

--- random_maze.py
+++ random_maze.py
@@ -4,6 +4,7 @@ import turtle
 
 def check(x, y, number):
     count = 0
+    become = -1
     if (x <= size - 1 and x >= 0) and (y <= size - 1 and y >= 0):
         if ground[y + 1][x] == number:
             count += 1
@@ -18,25 +19,29 @@ def check(x, y, number):
 
         if ground[y + 1][x] != number and ground[y + 1][x] != 0:
             count += 10
+            become = ground[y + 1][x]
         elif ground[y][x + 1] != number and ground[y][x + 1] != 0:
             count += 10
+            become = ground[y][x + 1]
         elif ground[y - 1][x] != number and ground[y - 1][x] != 0:
             count += 10
+            become = ground[y - 1][x]
         elif ground[y][x - 1] != number and ground[y][x - 1] != 0:
             count += 10
-    return count
+            become = ground[y][x - 1]
+    return (count, become)
 
 #turn = [go_up + 1, go_right + 1, go_up - 1, go_right - 1]  ###
 def road(x, y, number, ground, position):
     #print("--------------", number)
     turn = [0, 1, 2, 3]
-    ok = 0
-    while ok != 11 and turn != []:
+    ok = (0, -1)
+    while ok[0] != 11 and turn != []:
         r = random.choice(turn)   #r=random
         #print(">", r)
         if r == 0:  #y + 1
             ok = check(x, y + 1, number)
-            if ok == 1 or ok == 11:
+            if ok[0] == 1 or ok[0] == 11:
                 y += 1
                 ground[y][x] = number
                 position.append((x, y))
@@ -45,7 +50,7 @@ def road(x, y, number, ground, position):
                 turn.remove(r)
         elif r == 1:  #x + 1
             ok = check(x + 1, y, number)
-            if ok == 1 or ok == 11:
+            if ok[0] == 1 or ok[0] == 11:
                 x += 1
                 ground[y][x] = number
                 position.append((x, y))
@@ -54,7 +59,7 @@ def road(x, y, number, ground, position):
                 turn.remove(r)
         elif r == 2:  #y - 1
             ok = check(x, y - 1, number)
-            if ok == 1 or ok == 11:
+            if ok[0] == 1 or ok[0] == 11:
                 y -= 1
                 ground[y][x] = number
                 position.append((x, y))
@@ -63,7 +68,7 @@ def road(x, y, number, ground, position):
                 turn.remove(r)
         elif r == 3:  #x - 1
             ok = check(x - 1, y, number)
-            if ok == 1 or ok == 11:
+            if ok[0] == 1 or ok[0] == 11:
                 x -= 1
                 ground[y][x] = number
                 position.append((x, y))
@@ -71,32 +76,32 @@ def road(x, y, number, ground, position):
             else:
                 turn.remove(r)
         """
-        print(ok)
-        if ok == 11:
+        print(ok[0])
+        if ok[0] == 11:
             for i in range(size, -1, -1):
                 print(ground[i])
         print(position)
         """
-    if ok == 11:
+    if ok[0] == 11:  #碰到的數字不同時
         #print("break_exchange")
-        return 1
+        return (1, ok[1])
     else:
         #print("break")
-        return 0
+        return (0, ok[1])
 
 def find(number, position):
-    change_ok = 0
+    change_ok = (0, -1)
     x = -1
     y = -1
-    while change_ok != 3 and position != []:
+    while change_ok[0] != 3 and position != []:
         r_position = random.choice(position)   #r=random
         change_ok = check(r_position[0], r_position[1], number)
-        if change_ok == 3:
+        if change_ok[0] == 3:
             x = r_position[0]
             y = r_position[1]
             #print((x, y))
         position.remove(r_position)
-    return [x, y]
+    return (x, y)
 
 def exchange(delete, become, ground):
     #當不同數字連在一起時，改成相同數字
@@ -131,9 +136,14 @@ def changeline(size, distance):
 ###################
 size = 10  #迷宮大小
 distance = 10  #路的大小
+ending = 10  #出口數
 run = True
 ###################
 side = size * distance
+
+#畫路
+turtle.speed(0)
+turtle.hideturtle()
 
 
 A = [0] * (size + 1)
@@ -141,32 +151,77 @@ ground = []
 for i in range(size + 1):
     ground.append(A[:])
 
-x_first = random.randint(0, size - 1)
-y_first = random.randint(0, size - 1)
-position_first = [(x_first, y_first)]
-ground[y_first][x_first] = 1
+start = []
 
-x_final = random.randint(0, size - 1)
-y_final = random.randint(0, size - 1)
-position_final = [(x_final, y_final)]
-ground[y_final][x_final] = 2
+"""
+#隨機選
+Number = []
+for i in range(1, ending + 2):
+    Number.append(i)
 
-
-#畫路
-turtle.speed(0)
-turtle.hideturtle()
+position = [[]] * (ending + 1)
 
 #first
+x_first = random.randint(0, size - 1)
+y_first = random.randint(0, size - 1)
+position[0] = [(x_first, y_first)]
+start.append((x_first, y_first))
+ground[y_first][x_first] = 1
+
 turtle.penup()
 turtle.setposition(-1 * side / 2 + x_first * 10 + 1, -1 * side / 2 + y_first * 10 + 1)
 turtle.color(0, 1, 0)
 block(distance - 2)
 
+
 #final
+for i in range(ending):
+    position[i + 1].append([])
+    x_final = random.randint(0, size - 1)
+    y_final = random.randint(0, size - 1)
+    position[i + 1] = [(x_final, y_final)]
+    ground[y_final][x_final] = i + 2
+    start.append((x_final, y_final))
+    
+    turtle.penup()
+    turtle.setposition(-1 * side / 2 + x_final * 10 + 1, -1 * side / 2 + y_final * 10 + 1)
+    turtle.color(1, 0, 0)
+    block(distance - 2)
+"""
+#手動選
+ending = int(input("幾個出口（size = 10）："))
+
+Number = []
+for i in range(1, ending + 2):
+    Number.append(i)
+
+position = [[]] * (ending + 1)
+
+#first
+x_first = random.randint(0, size - 1)
+y_first = random.randint(0, size - 1)
+position[0] = [(x_first, y_first)]
+start.append((x_first, y_first))
+ground[y_first][x_first] = 1
+
 turtle.penup()
-turtle.setposition(-1 * side / 2 + x_final * 10 + 1, -1 * side / 2 + y_final * 10 + 1)
-turtle.color(1, 0, 0)
+turtle.setposition(-1 * side / 2 + x_first * 10 + 1, -1 * side / 2 + y_first * 10 + 1)
+turtle.color(0, 1, 0)
 block(distance - 2)
+
+
+#final
+for i in range(ending):
+    x_final, y_final = eval(input("x, y :"))
+    position[i + 1] = [(x_final, y_final)]
+    ground[y_final][x_final] = i + 2
+    start.append((x_final, y_final))
+    
+    turtle.penup()
+    turtle.setposition(-1 * side / 2 + x_final * 10 + 1, -1 * side / 2 + y_final * 10 + 1)
+    turtle.color(1, 0, 0)
+    block(distance - 2)
+"""#"""
 
 turtle.penup()
 turtle.color(0, 0, 0)
@@ -174,32 +229,30 @@ turtle.setposition(-1 * side / 2, -1 * side / 2)
 
 
 while run == True:
-    if position_first == []:
+    if position[0] == []:
         print("all_break")
         break
     else:
-        if position_final == []:
-            road(x_first, y_first, 1, ground, position_first)
-            x_first, y_first = find(1, position_first)
+        change = (0, -1)
+        for number in Number:
+            x = start[number - 1][0]
+            y = start[number - 1][1]
+            change = road(x, y, number, ground, position[number - 1])
+            if change[0] == 1:
+                if number == 1:
+                    exchange(change[1], 1, ground)
+                    position[0] += position[change[1] - 1]
+                    position[change[1] - 1] = []
+                    Number.remove(change[1])
+                else:
+                    exchange(number, change[1], ground)
+                    position[change[1] - 1] += position[number - 1]
+                    position[number - 1] = []
+                    Number.remove(number)
 
-        else:
-            change = 0
-            change += road(x_first, y_first, 1, ground, position_first)
-            if change == 1:
-                exchange(2, 1, ground)
-                position_first += position_final
-                position_final = []
-                continue
-                
-            change += road(x_final, y_final, 2, ground, position_final)
-            if change == 1:
-                exchange(2, 1, ground)
-                position_first += position_final
-                position_final = []
-                continue
-            
-            x_first, y_first = find(1, position_first)
-            x_final, y_final = find(2, position_final)
+        for number in Number:
+            start[number - 1] = find(number, position[number - 1])
+
 
 '''
 for i in range(random.randint(1, size)):
@@ -210,8 +263,8 @@ for i in range(random.randint(1, size)):
 """
 for i in range(size, -1, -1):
     print(ground[i])
-print(position_first)
-"""#"""
+print(position[0])
+"""
 
 
 #畫路
