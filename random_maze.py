057802f21a7a1aef71b8e603339290b2a843f1f4
IalvinchangI <99197625+IalvinchangI@users.noreply.github.com>
2025-01-30 19:26:37 +0800

    偽裝成QRcode的迷宮

--- random_maze.py
+++ random_maze.py
@@ -23,89 +23,7 @@ class maze():
         self.position = {self.ground[i[1]][i[0]]:[i] for i in self.start}
         self.Number = sorted(list(self.position))
         self.max_number = self.Number[-1] if self.Number else 0
-
-    '''
-    def add_struct(self, x, y, number, struct):
-        """
-        可加，就加struct
-        不可加，就加(x, y)
-        """
-        # struct === obj
-        ok = self.__check_struct(x, y, number, struct)
-        if ok[0]:
-            print(True)
-            for number_s_n, become_list in enumerate(ok[3]):
-                number_s = struct.Number[number_s_n]
-                if len(become_list) > 1:  # 有碰到很多東西
-                    if 1 in become_list:
-                        number = 1
-                    else:
-                        number = become_list[0]  # become的第1個是"要變成的"
-                    for i in range(1, len(become_list)):
-                        self.__exchange(become_list[i], number)
-                elif len(become_list) == 1:  # 有碰到1個東西
-                    number = become_list[0]
-                else:  # 沒有碰到東西
-                    self.max_number += 1
-                    number = self.max_number
-                    self.Number.append(number)
-                    self.position[number] = []
-                for (sx, sy), (gx, gy) in zip(ok[1][number_s], ok[2][number_s_n]):  # 把struct印到ground上
-                    if struct.ground[sy][sx] == number_s:
-                        self.ground[gy][gx] = number
-                        self.position[number].append((gx, gy))
-        else:
-            print(False)
-            self.ground[y][x] = number
-            self.position[number].append((x, y))
     
-    def __check_struct(self, x, y, number, struct):
-        # struct === obj
-        S = copy.deepcopy(struct.ground)
-        save_s = copy.deepcopy(struct.position)
-        save_g = [[(x + struct.start[i][0] - struct.start[0][0], y + struct.start[i][1] - struct.start[0][1])] for i in range(len(struct.Number))]  # <<< enumerate(self.Number)
-        # save_self_pos = [[(x + pos[0] - struct.start[0][0], y + pos[1] - struct.start[0][1]) for pos in struct.position[i]] for i in struct.Number]
-        # follow_numbers = []
-        become = []  # become的第1個是"要變成的"
-        for i in range(len(struct.Number)):
-            become.append([])
-        try:
-            for number_s_n, number_s in enumerate(struct.Number):  # change
-                i = 0
-                while len(save_s[number_s]) > i:
-                    print(i)
-                    sx, sy = save_s[number_s][i]
-                    gx, gy = save_g[number_s_n][i]
-                    for x_, y_ in self.get_cross_pos(0, 0):
-                        # struct內
-                        if (sx + x_ < struct.xd and sx + x_ >= 0) and (sy + y_ < struct.yd and sy + y_ >= 0):
-                            if S[sy + y_][sx + x_] == 0 or S[sy + y_][sx + x_] != number_s:  #為0或不為nomber_s就不用檢查ground合不合了
-                                continue
-                        elif (sx + x_ > struct.xd and sx + x_ < -1) and (sy + y_ > struct.yd and sy + y_ < -1):
-                            continue
-                        # ground內
-                        gx_, gy_ = gx + x_, gy + y_
-                        if (gx_ >= self.xd and gx_ < 0) and (gy_ >= self.yd and gy_ < 0):
-                            raise Exception("can't add struct")
-                        if self.ground[gy_][gx_] in become[number_s_n - 1]:
-                            raise Exception("can't add struct")
-                        if (self.ground[gy_][gx_] not in become[number_s_n - 1]) and (self.ground[gy_][gx_] != 0):
-                            # if follow_numbers:  # 碰過東西了  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-                            become[number_s - 1].append(self.ground[gy_][gx_])
-                            # follow_numbers.append(self.ground[gy_][gx_])
-                        save_s[number_s].append((sx + x_, sy + y_))
-                        save_g[number_s_n].append((gx_, gy_))
-                        S[sy + y_][sx + x_] = 0
-                    i += 1
-                # follow_numbers = []
-        except:
-            return (False, None, None, None)
-        else:
-            del save_s[struct.Number[0]][0]
-            del save_g[0][0]
-            return (True, save_s, save_g, become)
-    '''
-
     def add_struct(self, x, y, struct, make_x_y_TF = False):
         """ (x, y) 不能有 number """
         ok = self.__check_struct(x, y, struct)
@@ -232,19 +150,10 @@ class maze():
 
     def get_cross_pos(self, x, y, center_TF = False):
         # 上右下左(中)的資料
-        #'''
         if center_TF:
             return ((x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y), (x, y))
         else:
             return ((x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y))
-        '''
-        yield (x, y + 1)
-        yield (x + 1, y)
-        yield (x, y - 1)
-        yield (x - 1, y)
-        if check_exchage_TF == False:
-            yield (x, y)
-        #'''
 
     def __check(self, x, y, number):
         count = 0
@@ -366,16 +275,13 @@ def create_list2(xd, yd):
 
 def create_in_out(maze_obj, ending, random_TF = True, construct_structs = None):
     # 製作出入口
+    QR_pos = [(7, 7), (7, maze_obj.yd - 8), (maze_obj.xd - 8, maze_obj.yd - 8), (maze_obj.xd - 8, 5), (1, 1), (1, maze_obj.yd - 6), (maze_obj.xd - 6, maze_obj.yd - 6)]
     for number in range(1, ending + 2):
         while True:
             if random_TF:  #隨機選
-                x = random.randint(0, maze_obj.xd - 1)
-                y = random.randint(0, maze_obj.yd - 1)
-                pos = (x, y)
-                if pos in maze_obj.start:
-                    continue
-                if construct_structs:  #隨機放置結構
-                    struct_number = random.randint(0, len(construct_structs) - 1)
+                pos = QR_pos[number - 1]
+                x, y = pos
+                struct_number = number - 1 if number <= 4 else 4
             else:  #手動選
                 i = 1
                 try:
@@ -428,6 +334,7 @@ def draw(maze_obj, distance):
     y_side = maze_obj.yd * distance
     #畫出入口
     turtle.penup()
+    """
     for i in range(len(maze_obj.start)):
         turtle.setposition(-1 * x_side / 2 + maze_obj.start[i][0] * distance + 1, -1 * y_side / 2 + maze_obj.start[i][1] * distance + 1)
         if i == 0:
@@ -435,7 +342,7 @@ def draw(maze_obj, distance):
         else:
             turtle.color(1, 0, 0)
         block(distance - 2)
-    
+    #"""
     #畫路
     turtle.color(0, 0, 0)
     turtle.setposition(-1 * x_side / 2, -1 * y_side / 2)
@@ -458,73 +365,44 @@ def draw(maze_obj, distance):
     turtle.penup()
 
 #########################################################
-i = 1
-while True:
-    try:
-        if i == 1:
-            xd = int(input("迷宮長度(x軸)："))   #迷宮大小(x軸)   185
-            if xd <= 0:
-                print("資料值錯誤，請重新輸入")
-                continue
-            i += 1
-        if i == 2:
-            yd = int(input("迷宮寬度(y軸)："))   #迷宮大小(y軸)   95
-            if yd <= 0:
-                print("資料值錯誤，請重新輸入")
-                continue
-            Max_ending = xd * yd - 1
-            i += 1
-        if i == 3:
-            distance = int(input("路的大小："))  #路的大小   8
-            if distance <= 0:
-                print("資料值錯誤，請重新輸入")
-                continue
-            i += 1
-        if i == 4:
-            ending = int(input("幾個出口："))    #出口數   20
-            if ending > Max_ending:
-                print("資料值大於%d，請重新輸入" % Max_ending)
-                continue
-            i += 1
-        if i == 5:
-            random_TF = bool(eval(input("是否隨機選擇起終點座標 (True、False)：")))  #隨機選擇起終點座標  True
-            del i
-    except ValueError:
-        print("資料型態錯誤，請重新輸入")
-    except Exception:
-        print("輸入錯誤，請重新輸入")
-    else:
-        break
+xd = 35
+yd = 35
+distance = 20
+ending = 6
+random_TF = True
 #########################################################
 
 turtle.tracer(0, 0)
 
 A_55 = [[-1, -1, -1, -1, -1], 
-        [-1, 1, 1, 1, -1], 
-        [-1, 1, -1, 1, 0], 
-        [-1, 1, 1, 1, -1], 
+        [-1,  1,  1,  1, -1], 
+        [-1,  1, -1,  1, -1], 
+        [-1,  1,  1,  1, -1], 
         [-1, -1, -1, -1, -1]]
 
-B_55 = [[1, 1, 1, 1, 1], 
-        [-1, -1, -1, -1, -1], 
-        [2, 2, 2, -1, 3], 
-        [2, -1, -1, -1, 3], 
-        [2, 2, 2, 3, 3]]
-
-A_77 = [[-1, -1, -1, 0, -1, -1, -1], 
-        [-1, 1, 1, 1, 1, 1, -1], 
-        [-1, 1, -1, -1, -1, 1, -1], 
-        [-1, 1, -1, -1, -1, 1, 0], 
-        [-1, 1, -1, -1, -1, 1, -1], 
-        [-1, 1, 1, 1, 1, 1, -1], 
+A_77 = [[-1, -1, -1, -1, -1, -1, -1], 
+        [-1,  1,  1,  1,  1,  1, -1], 
+        [-1,  1, -1, -1, -1,  1, -1], 
+        [-1,  1, -1, -1, -1,  1, -1], 
+        [-1,  1, -1, -1, -1,  1, -1], 
+        [-1,  1,  1,  1,  1,  1, -1], 
         [-1, -1, -1, -1, -1, -1, -1]]
 
+B_88 = [[1, 1, 1, 1, 1, 1, 1, 1], 
+        [0, 0, 0, 0, 0, 0, 0, 1], 
+        [0, 0, 0, 0, 0, 0, 0, 1], 
+        [0, 0, 0, 0, 0, 0, 0, 1], 
+        [0, 0, 0, 0, 0, 0, 0, 1], 
+        [0, 0, 0, 0, 0, 0, 0, 1], 
+        [0, 0, 0, 0, 0, 0, 0, 1], 
+        [0, 0, 0, 0, 0, 0, 0, 1]]
 
-construct_structs = [maze(copy.deepcopy(A_55), [(1, 1)]), maze(copy.deepcopy(B_55)[::-1], [(0, 0)]), maze(copy.deepcopy(A_77), [(1, 1)])]
+
+construct_structs = [maze(copy.deepcopy(B_88)[::-1], [(7, 7)]), maze(copy.deepcopy(B_88), [(7, 0)]), maze([i[::-1] for i in B_88], [(0, 0)]), 
+                     maze(A_55, [(1, 1)]), maze(A_77, [(1, 1)])]
 
 ground = maze(create_list2(xd, yd))
 create_in_out(ground, ending, random_TF, construct_structs)
-# create_in_out(ground, ending, random_TF)
 
 ground.create_random_maze()
 
